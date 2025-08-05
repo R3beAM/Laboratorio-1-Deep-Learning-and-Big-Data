2f1e7eb353f2d168c4aca9caa071cb9206b74c64
R3beAM <ralvarezmon@ucenfotec.ac.cr>
2025-08-05 16:55:22 -0600

    Update consumer.py

--- hbase_analytics/consumer.py
+++ hbase_analytics/consumer.py
@@ -26,7 +26,7 @@ table = connection.table('ventas')
 # Insertar datos fila por fila
 for i, row in df.iterrows():
     table.put(f"row{i}", {
-        b'info:category': str(row["category"]).encode(),
+        b'info:category': str(row["category_id"]).encode(),
         b'info:brand': str(row["brand"]).encode(),
         b'info:price': str(row["price"]).encode(),
         b'info:event_time': str(row["event_time"]).encode()
