148f3d4bdc8500d58d43dfd5153ed837bf28b304
R3beAM <ralvarezmon@ucenfotec.ac.cr>
2025-08-05 14:16:04 -0600

    Update consumer.py

--- hbase_analytics/consumer.py
+++ hbase_analytics/consumer.py
@@ -20,8 +20,8 @@ df = pd.read_csv('datos_kaggle/kz_cleaned.csv')
 
 for idx, row in df.iterrows():
     row_key = f"row{idx}"
-    table.put(row_key, {
-        b'info:category': str(row['category']).encode(),
+    table.put(f'row{i}', {
+        b'info:category': str(row['category']).encode(),  # ✅ valor str convertido a bytes
         b'info:brand': str(row['brand']).encode(),
         b'info:price': str(row['price']).encode(),
         b'info:event_time': str(row['event_time']).encode()
