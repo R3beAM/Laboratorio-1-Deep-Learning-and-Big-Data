89fc6267a5fe8b6635a350c14537dd335d1d5ee8
R3beAM <ralvarezmon@ucenfotec.ac.cr>
2025-08-07 12:20:02 -0600

    Create hbase_script.py

---
+++ hbase_script.py
@@ -0,0 +1,37 @@
+import happybase
+import pandas as pd
+from collections import Counter
+from datetime import datetime
+import time
+import csv
+
+#Paso 1: Cargar csv
+hbase thrift start
+
+# Conexión a HBase vía Thrift (puerto 9090 por defecto)
+connection = happybase.Connection('localhost', port=9090)
+table_name = 'ventas'
+
+# Crear tabla si no existe
+families = {
+    'data': dict()
+}
+
+if table_name.encode() not in connection.tables():
+    connection.create_table(table_name, families)
+
+table = connection.table(table_name)
+
+# Leer el CSV
+with open('kz_cleaned.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    
+    for i, row in enumerate(reader):
+        row_key = f"row{i}"
+        data = {
+            f"data:{key}": value
+            for key, value in row.items()
+        }
+        table.put(row_key, data)
+
+print("Datos cargados exitosamente en HBase.")
