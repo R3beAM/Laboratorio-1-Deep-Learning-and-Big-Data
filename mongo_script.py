9010ad80fd25accc606f51862c3b7697e1b9f35a
R3beAM <ralvarezmon@ucenfotec.ac.cr>
2025-08-07 12:21:15 -0600

    Create mongo_script.py

---
+++ mongo_script.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+
+
+#Primer paso: Cargar csv
+df = pd.read_csv('kz.csv')
+print("\n✅ Primeras filas del dataset:\n")
+print(df.head())
+
+
+
+#Conectar a la base de datos
+client = pymongo.MongoClient('mongodb://localhost:27017/', serverSelectionTimeoutMS=5000)
+
+
+#Crear la coleccion e insertamos los datos
+db = client['kz_database']
+collection = db['kz_collection']
+if collection.count_documents({}) == 0:
+    collection.insert_many(df.to_dict(orient='records'))
+    print("✅ Datos insertados en MongoDB")
+else:
+    print("ℹ️ Los datos ya estaban insertados")
+
+#-------------------------------------------------------------------------    
+#Consultar los datos
+# ------------------------------------------------------------------------------
+# 1️⃣ ¿Cuál es la categoría más vendida?
+# MongoDB Query:
+# db.ventas.aggregate([
+#   { $match: { category_code: { $ne: null } } },
+#   { $group: { _id: "$category_code", total: { $sum: 1 } } },
+#   { $sort: { total: -1 } },
+#   { $limit: 1 }
+# ])
+# ------------------------------------------------------------------------------
+resultado = collection.aggregate([
+    {"$match": {"category_code": {"$ne": None}}},
+    {"$group": {"_id": "$category_code", "total": {"$sum": 1}}},
+    {"$sort": {"total": -1}},
+    {"$limit": 1}
+])
+for doc in resultado:
+    print(f"Categoría más vendida: {doc['_id']} con {doc['total']} ventas")
+# ------------------------------------------------------------------------------
+# 2️⃣ ¿Qué marca generó más ingresos brutos?
+# MongoDB Query:
+# db.ventas.aggregate([
+#   { $match: { brand: { $ne: null } } },
+#   { $group: { _id: "$brand", total_ingresos: { $sum: "$price" } } },
+#   { $sort: { total_ingresos: -1 } },
+#   { $limit: 1 }
+# ])
+# -------------------------------------------------------------
+resultado = collection.aggregate([
+    {"$match": {"brand": {"$ne": None}}},
+    {"$group": {"_id": "$brand", "total_ingresos": {"$sum": "$price"}}},
+    {"$sort": {"total_ingresos": -1}},
+    {"$limit": 1}
+])
+for doc in resultado:
+    print(f"Marca con más ingresos: {doc['_id']} con ${doc['total_ingresos']} ventas")
+
+# ------------------------------------------------------------------------------
+# 3️⃣ ¿Qué mes tuvo más ventas? (en UTC)
+# MongoDB Query:
+# db.ventas.aggregate([
+#   {
+#     $project: {
+#       month: { $dateToString: { format: "%Y-%m", date: { $toDate: "$event_time" } } }
+#     }
+#   },
+#   {
+#     $group: {
+#       _id: "$month",
+#       total_ventas: { $sum: 1 }
+#     }
+#   },
+#   { $sort: { total_ventas: -1 } },
+#   { $limit: 1 }
+# ])
+#Modificar la fecha para formato datetime.datetime
+# ------------------------------------------------------------------------------
+resultado = collection.aggregate([
+   {
+        "$addFields": {
+            "event_date": {
+                "$dateFromString": {
+                    "dateString": "$event_time",
+                    "format": "%Y-%m-%d %H:%M:%S %Z",
+                    "timezone": "UTC"
+                }
+            }
+        }
+    },
+    {
+        "$project": {
+            "mes": {"$month": "$event_date"},
+            "anio": {"$year": "$event_date"}
+        }
+    },
+    {
+        "$group": {
+            "_id": {"anio": "$anio", "mes": "$mes"},
+            "total_ventas": {"$sum": 1}
+        }
+    },
+    {"$sort": {"total_ventas": -1}},
+    {"$limit": 1}
+])
+
+for doc in resultado:
+    print(f"Mes con más ventas: {doc['_id']['mes']}/{doc['_id']['anio']} con {doc['total_ventas']} ventas")
+    
+ 
+client.close()
